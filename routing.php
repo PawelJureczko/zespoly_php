40f04714df47e04782c10b6b40edcfebb9a0aba0
Paweu <paloboo@o2.pl>
2020-01-02 20:32:26 +0100

    dodano routing i style

--- routing.php
+++ routing.php
@@ -3,8 +3,9 @@
 use core\App;
 use core\Utils;
 
-App::getRouter()->setDefaultRoute('hello'); #default action
+App::getRouter()->setDefaultRoute('BandList'); #default action
 //App::getRouter()->setLoginRoute('login'); #action to forward if no permissions
 
 Utils::addRoute('hello', 'HelloCtrl');
+Utils::addRoute('BandList','BandListCtrl');
 //Utils::addRoute('action_name', 'controller_class_name');
