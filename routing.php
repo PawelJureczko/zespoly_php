de6136f6cad9e71b870b4ed487e39b91434239f7
Paweu <paloboo@o2.pl>
2020-01-05 19:42:34 +0100

    usuniety default template i default ctrl

--- routing.php
+++ routing.php
@@ -6,6 +6,5 @@ use core\Utils;
 App::getRouter()->setDefaultRoute('BandList'); #default action
 //App::getRouter()->setLoginRoute('login'); #action to forward if no permissions
 
-Utils::addRoute('hello', 'HelloCtrl');
 Utils::addRoute('BandList','BandListCtrl');
 //Utils::addRoute('action_name', 'controller_class_name');
